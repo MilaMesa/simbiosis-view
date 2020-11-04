2c4ba117b2eca4a6b7664acb522447eec7d3ef7a
05valen <73319347+05valen@users.noreply.github.com>
2020-11-04 11:07:00 -0500

    05VALENTINA
    IMAGENES

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,12 @@ const About = () => (
         <p className='text-center'>
             Simbiosis es una app creada para fortalezer la comuicacion en el mundo textil entre proveedores y talleres textiles.
         </p>
+        < img style ={{height:200}} src ="imagenesV/HILOS.JPG" ></img>       <li></li>
+        < img style ={{height:200}} src ="imagenesV/PROVEEDOR.JPG" ></img>   <li>PROVEEDOR</li>
+        < img style ={{height:200}} src ="imagenesV/TALLER.JPG" ></img>      <li>TALLER</li>
+
     </div>
+
 );
 
 export default About;
