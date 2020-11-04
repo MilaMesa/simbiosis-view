a08952266725db21b335f02d3ea611d0221dd884
05valen <73319347+05valen@users.noreply.github.com>
2020-11-04 13:28:07 -0500

    05valen
    pg

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,33 +1,45 @@
 import React from 'react';
+import  './index.css';
 
 const About = () => (
     <div className='container'>
-        <h1 className='text-center'>Simbiosis Textil</h1>
+        <h1 className='text-center'>Bienvenido a Simbiosis Textil</h1>
         <p className='text-center'>
             Simbiosis es una app creada para fortalezer la comuicacion en el mundo textil entre proveedores y talleres textiles.
         </p>
         < img style ={{height:200}} src ="imagenesV/HILOS.JPG" ></img>    
         < img style ={{height:200}} src ="imagenesV/TELAS.JPG" ></img> 
-        < img style ={{height:200}} src ="imagenesV/TELAS1.JPG" ></img> 
-        <h4><p>Proveedor </p></h4>
-        <p> Tienen la función básica de proporcionar materias primas, 
-            a la empresa con la que contrata. 
-            Su papel intermediador les confiere un potencial protagonismo,
-            pues tienen muchas formas eficaces de ejercer presión sobre sus clientes
-            para conseguir que se acepten sus condiciones
-       </p>
-        < img style ={{height:200}} src ="imagenesV/PROVEEDOR.JPG" ></img>
-        <h4><p>Taller </p></h4>  
-        <p>  En enseñanza, es una metodología de trabajo que se caracteriza por la investigación,
-             el aprendizaje por descubrimiento y el trabajo en equipo que, en su aspecto externo, 
-             se distingue por el acopio (en forma sistematizada) de material especializado acorde con
-             el tema tratado teniendo como fin la elaboración de un producto tangible.
-       </p>
+        < img style ={{height:200}} src ="imagenesV/TELAS1.JPG" ></img> <br />
 
-   
-        < img style ={{height:200}} src ="imagenesV/TALLER.JPG" ></img>      
-
+       
+        <table>
+                    <br /> 
+                <tr>
+                    <td>< img style ={{height:188}} src ="imagenesV/PROVEEDOR.JPG" ></img> </td>
+                    
+                    <td> <br /> <h4>Proveedor. </h4>
+                           Tienen la función básica de proporcionar materias primas, 
+                            a la empresa con la que contrata. 
+                            Su papel intermediador les confiere un potencial protagonismo,
+                            pues tienen muchas formas  eficaces 
+                            de ejercer presión sobre sus clientes
+                            para conseguir que se acepten sus condiciones.<br />
+                    </td> 
+                </tr>
+               
+                <br />
+                <tr>
+                    <td>< img style ={{height:200}} src ="imagenesV/TALLER.JPG" ></img>   </td>
+                    
+                    <td>  <h4>Taller. </h4>    
+                       Tiene la función de adquirir  los insumos o productos del proveedor con el fin de poder relizar sus produtos.<br />
+                    </td> 
+                </tr>
+     </table>
+         
     </div>
+
+    
 
 );
 
