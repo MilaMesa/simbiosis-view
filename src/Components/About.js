37e69829eb73a73603be7145ab330125af742544
05valen <73319347+05valen@users.noreply.github.com>
2020-11-04 11:52:20 -0500

    05VALEN
    ACERCA DE

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,9 +6,26 @@ const About = () => (
         <p className='text-center'>
             Simbiosis es una app creada para fortalezer la comuicacion en el mundo textil entre proveedores y talleres textiles.
         </p>
-        < img style ={{height:200}} src ="imagenesV/HILOS.JPG" ></img>       <li></li>
-        < img style ={{height:200}} src ="imagenesV/PROVEEDOR.JPG" ></img>   <li>PROVEEDOR</li>
-        < img style ={{height:200}} src ="imagenesV/TALLER.JPG" ></img>      <li>TALLER</li>
+        < img style ={{height:200}} src ="imagenesV/HILOS.JPG" ></img>    
+        < img style ={{height:200}} src ="imagenesV/TELAS.JPG" ></img> 
+        < img style ={{height:200}} src ="imagenesV/TELAS1.JPG" ></img> 
+        <h4><p>Proveedor </p></h4>
+        <p> Tienen la función básica de proporcionar materias primas, 
+            a la empresa con la que contrata. 
+            Su papel intermediador les confiere un potencial protagonismo,
+            pues tienen muchas formas eficaces de ejercer presión sobre sus clientes
+            para conseguir que se acepten sus condiciones
+       </p>
+        < img style ={{height:200}} src ="imagenesV/PROVEEDOR.JPG" ></img>
+        <h4><p>Taller </p></h4>  
+        <p>  En enseñanza, es una metodología de trabajo que se caracteriza por la investigación,
+             el aprendizaje por descubrimiento y el trabajo en equipo que, en su aspecto externo, 
+             se distingue por el acopio (en forma sistematizada) de material especializado acorde con
+             el tema tratado teniendo como fin la elaboración de un producto tangible.
+       </p>
+
+   
+        < img style ={{height:200}} src ="imagenesV/TALLER.JPG" ></img>      
 
     </div>
 
